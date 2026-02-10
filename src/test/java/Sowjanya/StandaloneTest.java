4e1fba9b185b199e77a96d606d8f9ccc2ce2ca81
chirlasowjanyareddy <sowjanyareddychirla@gmail.com>
2026-02-10 23:42:36 +0530

    Standalone test for framework

--- src/test/java/Sowjanya/StandaloneTest.java
+++ src/test/java/Sowjanya/StandaloneTest.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import javax.swing.*;
 import java.time.Duration;
 import java.util.List;
 
@@ -17,9 +21,36 @@ public class StandaloneTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/client");
+        driver.manage().window().maximize();
         driver.findElement(By.id("userEmail")).sendKeys("chirlasowjanyareddy@gmail.com");
         driver.findElement(By.id("userPassword")).sendKeys("Sowjanyareddy@123");
         driver.findElement(By.id("login")).click();
-        List<WebElement> products = driver.findElements(By.cssSelector("col-lg-4"));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".col-lg-4")));
+        String product_name ="ZARA COAT 3";
+        List<WebElement> products = driver.findElements(By.cssSelector(".col-lg-4"));
+        WebElement prod = products.stream().filter(product
+                ->product.findElement(By.cssSelector("b")).getText().equals(product_name)).findFirst().orElse(null);
+        prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("toast-container")));
+//        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+
+        driver.findElement(By.cssSelector("[routerlink*='/dashboard/cart']")).click();
+        List<WebElement> cartprods = driver.findElements(By.cssSelector(".cartSection h3"));
+        Boolean match = cartprods.stream().anyMatch(cartprod->cartprod.getText().equalsIgnoreCase(product_name));
+        Assert.assertTrue(match);
+        driver.findElement(By.cssSelector(".subtotal button")).click();
+
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")),"India").build().perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        driver.findElement(By.cssSelector(".ta-item:nth-last-of-type(2)")).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
+        String conform_msg = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        Assert.assertTrue(conform_msg.equalsIgnoreCase("Thankyou for the order."));
+        driver.close();
+
     }
 }
