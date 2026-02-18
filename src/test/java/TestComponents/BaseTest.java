45d57766cfb19611c4bbbcc8d85cb84d951bec11
chirlasowjanyareddy <sowjanyareddychirla@gmail.com>
2026-02-18 23:46:30 +0530

    maven terminal run

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -31,12 +32,19 @@ public class BaseTest {
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//main//java//Sowjanya//resources//GlobalData.properties");
         prop.load(fis);
-        String browsername = prop.getProperty("browser");
+        // to read browser value which is comming from maven
+        String browsername = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
+//        String browsername = prop.getProperty("browser");
 
         if(browsername.equalsIgnoreCase("chrome"))
         {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
+        }
+        else if (browsername.equalsIgnoreCase("Firefox"))
+        {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
